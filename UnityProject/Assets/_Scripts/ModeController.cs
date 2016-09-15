bb9302e3dde4872cf48b4f1da049b3e0abdd5a40
Jing Yi Wang <jingyilogin@outlook.de>
2016-09-15 18:17:14 +0200

    Added: UI so switch between view and edit mode

---
+++ UnityProject/Assets/_Scripts/ModeController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModeController : MonoBehaviour {
+
+    public enum Mode
+    {
+        Edit,
+        View
+    };
+
+    [SerializeField] private GameObject EditUI;
+    [SerializeField] private GameObject ViewUI;
+
+    private Mode mode = Mode.View;
+
+    public void SwitchMode(Mode newMode)
+    {
+
+    }
+
+}
